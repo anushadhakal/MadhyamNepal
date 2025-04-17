40f14199ba85b01eacb378d4253284ec4225c68c
Anusha Dhakal <anushadhakal10@gmail.com>
2025-04-17 19:14:15 +0545

    minor changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -32,6 +32,8 @@ const Footer = () => {
               <ul className={styles.links}>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About Us</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#team">Team</a></li>
                 <li><a href="#contact">Contact</a></li>
               </ul>
             </div>
